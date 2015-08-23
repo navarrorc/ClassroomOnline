c2a006237f414a16d53fc82bab4e8cb82f0405c0
Robert <rnavarro70@gmail.com>
2015-08-23 13:02:31 -0500

    Initial Load

---
+++ ClassroomOnline/Features/Feature1/Feature1.feature
@@ -0,0 +1,12 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="d83a0a38-69fa-4e25-a03f-ae74c180d160" description="SharePoint App Feature" featureId="3bce5b4b-55a6-45b8-ba72-df330b465edf" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="ClassroomOnline Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="8306fdf5-50a2-44cc-87b4-70d4433b3c35" />
+    <projectItemReference itemId="90e6df20-7453-4645-8557-a329562d5588" />
+    <projectItemReference itemId="cf2e4576-6a93-427d-bc69-2ac4c618679b" />
+    <projectItemReference itemId="da40699f-d6ad-475a-a9cd-fc8fb0014084" />
+    <projectItemReference itemId="4beb67a3-7ddd-4cc0-ac4a-416b50002af0" />
+    <projectItemReference itemId="c2de61f3-7a2f-4011-8ae3-88ed0dc633b5" />
+    <projectItemReference itemId="a5bb8525-b153-4bf9-9cfb-5ffc6ca8bb3c" />
+  </projectItems>
+</feature>
---
+++ ClassroomOnline/Scripts/App.js
@@ -0,0 +1,57 @@
+﻿'use strict';
+
+var context = SP.ClientContext.get_current();
+var user = context.get_web().get_currentUser();
+
+(function () {
+
+  // This code runs when the DOM is ready and creates a context object which is 
+  // needed to use the SharePoint object model
+  var collListItems;
+  $(document).ready(function () {
+    getConfigValues();
+  });
+
+  function getConfigValues() {
+    var context = SP.ClientContext.get_current();
+    var configList =
+      context.get_web().get_lists().getByTitle('Configuration Values');
+
+    var camlQuery = new SP.CamlQuery();
+    collListItems = configList.getItems(camlQuery);
+    context.load(collListItems);
+    context.executeQueryAsync(onGetConfigValuesSuccess, onGetConfigValuesFail);
+  }
+
+  function onGetConfigValuesSuccess() {
+    var OrgLogoUrl;
+    var OrgName;
+    var listItemEnumerator = collListItems.getEnumerator();
+
+    while (listItemEnumerator.moveNext()) {
+      var oListItem = listItemEnumerator.get_current();
+      var current = oListItem.get_item('Title');
+      switch (current) {
+        case 'OrganizationName':
+          OrgName = oListItem.get_item('Value');
+          break;
+        case 'OrganizationLogoUrl':
+          OrgLogoUrl = oListItem.get_item('Value');
+          break;
+      };
+    }
+    if (OrgName && OrgName.length > 0) {
+      $('#DeltaPlaceHolderPageTitleInTitleArea').html(OrgName);
+      $('.ms-siteicon-img').attr('title', OrgName);
+    }
+    if (OrgLogoUrl && OrgLogoUrl.length > 0) {
+      $('.ms-siteicon-img').attr('src', OrgLogoUrl);
+    } else {
+      $('.ms-siteicon-img').attr('src', '../Images/AppLogo.png');
+    }
+  }
+
+  function onGetConfigValuesFail(sender, args) {
+    alert('Failed to get Configuration Values. Error:' + args.get_message());
+  }
+})();
